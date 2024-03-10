fe0336a285c40853b2d07fc321874a3baf43df23
majidsaadati <majidsaadati021@gmail.com>
2024-03-10 19:59:41 +0330

    add cside bar "be sakhti"

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,44 @@
+import { Outlet, Link } from "react-router-dom";
+
+import { CSidebar, CSidebarBrand,CSidebarNav,CNavTitle,CNavItem ,CNavGroup,CBadge,CSidebarToggler } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import  {cilPuzzle} from '@coreui/icons'
+import Blogs from "./Blogs";
+
+const Layout = () => {
+  return (
+    <>
+      <CSidebar >
+  <CSidebarBrand>Sidebar Brand</CSidebarBrand>
+  <CSidebarNav>
+    <CNavTitle>Nav Title</CNavTitle>
+    <Link to={"/Blogs"}>
+    <CNavItem href="#">
+      <CIcon customClassName="nav-icon" icon={cilPuzzle} />
+      Nav item
+    </CNavItem>
+    
+    </Link>
+    <CNavItem href="#">
+      <CIcon customClassName="nav-icon" icon={cilPuzzle} />
+      With badge
+      <CBadge color="primary ms-auto">NEW</CBadge>
+    </CNavItem>
+    <CNavGroup toggler="Nav dropdown">
+      <CNavItem href="#">
+        <CIcon customClassName="nav-icon" icon={cilPuzzle} /> Nav dropdown item
+      </CNavItem>
+      <CNavItem href="#">
+        <CIcon customClassName="nav-icon" icon={cilPuzzle} /> Nav dropdown item
+      </CNavItem>
+    </CNavGroup>
+  </CSidebarNav>
+  <CSidebarToggler />
+</CSidebar>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
