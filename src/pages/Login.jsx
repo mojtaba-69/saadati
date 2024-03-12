4ca79ed3fea85f2806bddc857b166e2ca49ceca2
majidsaadati <majidsaadati021@gmail.com>
2024-03-12 16:44:27 +0330

    login v0.5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login(){
+    const [username, setUsername ] = useState("");
+    const [password, setpassword ] = useState("")
+
     return (
     <>
         <div className="bg-light h-100 w-100 d-flex justify-content-center align-items-center">
-        <div className="rounded-4 bg-primary p-5 d-flex flex-column w-50 justify-content-center align-items-center">
+        <div className="rounded-4 bg-primary bg-opacity-25 bg-gradient p-5 d-flex flex-column w-50 justify-content-center align-items-center">
            
-            <input className="rounded-3 mb-4 p-2 border border-0" type="text" placeholder="نام کاربری"/>
-            <p className="">اطلاعات خود را به درستی وارد نمایید</p>
-            <input className="rounded-3 mb-4 p-2 border border-0" type="password" placeholder="رمز ورود"/>
-            <p className=""></p>
+            <input onInput={e=>setUsername(e.target.value)} className="rounded-3 mb-4 p-2 border border-0" type="text" placeholder="نام کاربری"/>
+            <p className="fs-6 fw-light">{username.length == "" ? "نام کاربری خود را واردد نمایید" : username.length !=4 ? "نامکاربری باید حداقل 4 کاراکتر باشد" : ""}</p>
+            <input onInput={e=>setpassword(e.target.value)} className="rounded-3 mb-4 p-2 border border-0" type="password" placeholder="رمز ورود"/>
+            <p className="fs-6 fw-light">{password.length == "" ? "رمز خود را واردد نمایید" : password.length !=8 ? "رمز باید حداقل 8 کاراکتر باشد" : ""}</p>
             <button className="rounded-3 p-2" type="submit"><Link className="link-secondary p-2 text-decoration-none" to={'/Home'}>ورود</Link> </button>   
         </div>
         </div>
